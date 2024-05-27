d6da0d8162b6d08e4201a2522da3cdc1b37beaa0
laveezah-noor <laveezahnoor@gmail.com>
2024-05-27 23:02:23 +0500

    fix: resume url problem

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -63,7 +63,7 @@ function Navbar() {
             ease: "easeInOut",
           }}
         >
-          <Link href={baseUrl}>
+          <Link href="/">
             <Logo />
           </Link>
         </motion.div>
@@ -124,7 +124,7 @@ function Navbar() {
               delay: 0.6,
             }}
           >
-            <Button text="Resume" link={`{baseUrl}/resume.pdf`} />
+            <Button text="Resume" link={`/resume.pdf`} />
           </motion.div>
         </div>
       </div>
